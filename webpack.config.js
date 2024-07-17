a7e432254d3f6a33f8a1b6ff0042f4d62440c62f
Kiwu <maxnight2014@gmail.com>
2024-07-17 23:20:51 +0300

    Added basic file structure

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  mode: "development",
+  devServer: {
+    static: {
+        directory: path.resolve(__dirname, 'dist')
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: 'src/favicon.ico',
+      title: 'To-Do App | ARU-Kiwu',
+      template: 'src/index.html'
+    }),
+  ],
+};
